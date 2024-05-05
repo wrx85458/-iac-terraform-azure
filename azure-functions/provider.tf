3c8673318696deb9a5e50d110e02453dae4d4fe8
wrx85458 <152792629+wrx85458@users.noreply.github.com>
2024-05-05 11:55:54 +0000

    Change provider config

--- azure-functions/provider.tf
+++ azure-functions/provider.tf
@@ -1,4 +1,18 @@
+terraform {
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  version = ">= 0.12"
+  skip_provider_registration = true 
   features {}
 }
