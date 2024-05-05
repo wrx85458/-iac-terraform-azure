537d505bd3455fdbe04401ceeae6d6138dd473ae
wrx85458 <152792629+wrx85458@users.noreply.github.com>
2024-05-05 11:35:48 +0000

    Add files

---
+++ azure-functions/main.tf
@@ -0,0 +1,44 @@
+resource "azurerm_resource_group" "test" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "test" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.storage_account_replication_type
+}
+
+resource "azurerm_app_service_plan" "test" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = var.app_service_plan_tier
+    size = var.app_service_plan_size
+  }
+}
+
+resource "azurerm_function_app" "test" {
+  name                      = var.function_app_name
+  location                  = azurerm_resource_group.test.location
+  resource_group_name       = azurerm_resource_group.test.name
+  app_service_plan_id       = azurerm_app_service_plan.test.id
+  storage_account_name      = azurerm_storage_account.test.name
+  storage_account_access_key = azurerm_storage_account.test.primary_access_key
+}
+
+resource "azurerm_function_app_function" "test" {
+  function_app_id  = azurerm_function_app.test.id
+  name             = "MyFunction"
+  storage_account_name = azurerm_storage_account.test.name
+  storage_account_access_key = azurerm_storage_account.test.primary_access_key
+  app_settings = {
+    "AzureWebJobsStorage" = azurerm_storage_account.test.primary_connection_string
+  }
+}
---
+++ azure-functions/variables.tf
@@ -0,0 +1,35 @@
+variable "resource_group_name" {
+  description = "test"
+}
+
+variable "location" {
+  description = "test"
+}
+
+variable "storage_account_name" {
+  description = "test"
+}
+
+variable "storage_account_tier" {
+  description = "test"
+}
+
+variable "storage_account_replication_type" {
+  description = "test"
+}
+
+variable "app_service_plan_name" {
+  description = "test"
+}
+
+variable "app_service_plan_tier" {
+  description = "test"
+}
+
+variable "app_service_plan_size" {
+  description = "test"
+}
+
+variable "function_app_name" {
+  description = "test"
+}
